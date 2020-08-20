bb9ebbe963e728ce2595e247e9128c084f47de58
WannaC <1477436670@qq.com>
2020-08-20 22:54:16 +0800

    first commit

---
+++ HelloGit.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println("Hello Git !---" + date.toString());
+    }
+}
